267b92db17de005224ea3fc9470c68401ab04983
Martin Grenouilloux <martin.grenouilloux@epita.fr>
2022-07-11 15:42:54 +0200

    soutenance

--- src/main.c
+++ src/main.c
@@ -15,33 +15,41 @@ int main(void)
 
     BIGNUM *prime = BN_new();
 
-    printf("generating prime...");
+    int prime_size = 4096;
+
+    printf("generating prime of size %d...", prime_size);
     fflush(stdout);
-    get_prime(prime, 1024);
+    get_prime(prime, prime_size);
 
     char *prime_str = BN_bn2dec(prime);
     printf(" found !\n %s\n", prime_str);
 
+    free(prime_str);
+
+    printf("---------------------------------------------------\n");
+    printf("generating rsa key pair...");
+    fflush(stdout);
     rsa_key *key = gen_rsa_key(1024);
+
+    printf(" generated !\n\n");
+
 
     char *p_ = BN_bn2dec(key->p);
     char *q_ = BN_bn2dec(key->q);
     char *e_ = BN_bn2dec(key->e);
-    char *d_ = BN_bn2dec(key->d);
     char *n_ = BN_bn2dec(key->n);
-    char *phi_ = BN_bn2dec(key->phi);
     printf("p = %s\n", p_);
     printf("q = %s\n", q_);
     printf("e = %s\n", e_);
-    printf("d = %s\n", d_);
     printf("n = %s\n", n_);
-    printf("phi = %s\n", phi_);
     free(p_);
     free(q_);
     free(e_);
-    free(d_);
     free(n_);
-    free(phi_);
+
+    printf("---------------------------------------------------\n");
+    printf("generating DH modulus...");
+    fflush(stdout);
 
     BIGNUM *dh_modulus = BN_new();
     gen_dh_modulus(dh_modulus, 1024);
@@ -52,7 +60,6 @@ int main(void)
     BN_free(dh_modulus);
 
     free_rsa_key(key);
-    free(prime_str);
     BN_free(prime);
     return 0;
 }
